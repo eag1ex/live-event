a1774e412fc89ca5ae8a556d3194781d52243c59
eag1ex <eaglex@eaglex.net>
2018-09-11 11:44:07 -0700

    update minify script

--- live.event.min.v1.0.0.js
+++ live.event.min.v1.0.0.js
@@ -2,7 +2,7 @@
  * LICENCE: CC BY
  * SOURCE: https://creativecommons.org/licenses/by/4.0/legalcode
  * 
- * 
+ * minified with : https://www.minifier.org/
 
  */
 
@@ -19,7 +19,10 @@ this.cbNow=()=>{this.timestamp=Date.now();const d={eventName:this.eventName,resp
 if(typeof this.__cb=='function'){if(this.checkHistory()===!0){return}
 if(d.resp){this.__cb(d)}
 this.index++}}
-this.fireEvent=()=>{this.cbNow()}}};delete(eventNames,cb){var check=eventNames.replace(/ /g,"");var have_multiple=check.split(",");var one_callback=1;var inst=have_multiple.map((env,inx)=>{if(this._instance[env])delete this._instance[env];if(this.watchInstance[env])delete this.watchInstance[env];if(!this.watchInstance[env]&&!this._instance[env]&&one_callback===1){console.debug(`eventName/watchInstance ${env} deleted!`)
+this.fireEvent=()=>{this.cbNow()}}};delete(eventNames,cb){if(!eventNames){console.debug(`eventName/watchInstance ${eventNames} not set!`)}
+var check=eventNames.replace(/\s/g,'');var have_multiple=check.split(",");var one_callback=1;var inst=have_multiple.map((env,inx)=>{if(!this.watchInstance[env]){console.debug(`no live eventName's found to delete!`)
+return}
+if(this._instance[env])delete this._instance[env];if(this.watchInstance[env])delete this.watchInstance[env];if(!this.watchInstance[env]&&!this._instance[env]&&one_callback===1){console.debug(`eventName/watchInstance ${env} deleted!`)
 if(typeof cb==='function'){setTimeout(()=>{cb(!0)},100)
 one_callback++}}})}
 lastPromise(_set=!0,env,data){if(_set&&!env){return Promise.reject('set is set but env is not')}
@@ -37,7 +40,7 @@ console.debug(`eventName ${event} Qued`)}
 if(dataForQue&&this._eventQue[event]&&!cb){this._eventQue[event].resp=dataForQue;this._eventQue[event].resp.delayed=!0;if(typeof final_cb==='function'){setTimeout(function(){final_cb(!0)},166)}}}
 callEvent(eventName,cb,watch=!1,dataForQue){if(!_.isString(eventName)){console.debug('eventName should be a string',eventName)
 return}
-var check=eventName.replace(/ /g,"");var have_multiple=check.split(",");var _t=this;var inst=have_multiple.map((env,inx)=>{if(!env){console.debug('no match for ',eventName)
+var check=eventName.replace(/\s/g,'');var have_multiple=check.split(",");var _t=this;var inst=have_multiple.map((env,inx)=>{if(!env){console.debug('no match for ',eventName)
 return}
 if(env){if(env===' '||env===','||env.length<3){return}}
 if(watch&&this.watchInstance[env]){cb({error:`eventName ${env} already registered`})
@@ -50,13 +53,18 @@ this._instance[env].eventName=env}
 if(cb){this._instance[env].__cb=cb;this._instance[env].fireEvent()}
 return this._instance[env]})
 return inst}
-watch(eventName,cb){if(!eventName){console.debug('watch, need to provice eventName');return!1}
-setTimeout(()=>{var evn;this.callEvent(eventName,(d)=>{if(!d.eventName){return}
+errorHandler(str,type='watch'){if(!str&&type==='dispatch'){console.debug('dispatch, need to provide eventName');return}
+if(!str&&type==='watch'){console.debug('watch, need to provide eventName');return!0}
+if(type==='dispatch'&&str.indexOf(',')!==-1){console.debug(`watch, eventName :'${str}' invalid setting`);return!0}
+if(!_.isString(str)){console.debug(`watch, eventName :${str} should be a string`);return!0}
+if(str.indexOf(',')!==-1&&str.length===1&&type==='watch'){console.debug(`watch, eventName :'${str}' invalid setting`);return!0}
+if(str.indexOf(' ')!==-1&&str.length===1){console.debug(`watch, eventName :'${str}' cannot be an empty space`);return!0}
+return!1}
+watch(eventName,cb){var err=this.errorHandler(eventName,'watch');if(err)return;setTimeout(()=>{var evn;this.callEvent(eventName,(d)=>{if(!d.eventName){return}
 if(d.eventName){this.watchInstance[d.eventName]=!0;evn=d.eventName}
 if(d.error){return}
 if(d.resp){return cb({resp:d.resp,eventName:evn,timestamp:d.timestamp})}},!0)},200)}
-dispatch(eventName,data){if(!eventName){console.debug('dispatch, need to provide eventName');return}
-if(typeof data==='string'){data={data:data}}
+dispatch(eventName,data){var err=this.errorHandler(eventName,'dispatch');if(err)return;if(typeof data==='string'){data={data:data}}
 var evnt=this.callEvent(eventName,!1,!1,data);if(!evnt.length){console.debug('liveEvent evnt error');return}
 var instances=evnt.map((item,inx)=>{if(item){item.resp=data;item.fireEvent();return item}})
 return instances}}
